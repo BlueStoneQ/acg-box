ecc35cb8d7a385309f34d87eb29940cb2f80f734
qiaoyang <qiaoyang@bonc.com.cn>
2018-08-20 09:18:19 +0800

    关于元素可编辑问题处理--测试失误-debugger 命令行参数设置有误

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import NavList from './router/NavList'
-import { editInit } from './utils'
 import '../style/less/comStyle/common.less'
 
 class App extends Component {
-  componentDidMount () {
-    editInit()
-  }
+
   render () {
     return (
       <div>
