bc730ad54b06c02fcd9eba4f913361cfb3e8964e
qiaoyang <qiaoyang@bonc.com.cn>
2018-09-07 17:09:54 +0800

    navBar中对虚拟键盘呼出的响应重构+全局事件销毁机制+关于TabBar上划隐藏的思路记录

--- src/js/components/navBar/NavBar.jsx
+++ src/js/components/navBar/NavBar.jsx
@@ -21,9 +21,6 @@ import PropTypes from 'prop-types'
 import NavItem from './NavItem'
 import '../../../style/less/componentStyle/navBar/navBar.less'
 
-/** 最初的屏幕可用尺寸 -- 测试 */
-const oHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-
 class NavBar extends React.Component {
   constructor (props) {
     super(props)
@@ -46,8 +43,29 @@ class NavBar extends React.Component {
     ))
   }
   /**
+   * 屏幕变化时的响应事件
+   * @param {obj} thiz 该函数执行的上下文对象，也就是NavBar组件本身
+   * @param {num} oHeight 原来的屏幕尺寸（文档尺寸）
+   */
+  hideNavBar (thiz, oHeight) {
+    const nHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    // 当屏幕的尺寸（宽/高）变化超过1/3 -- 现在只用高 -- 这里的比较还是需要采用其他值/或者公共变量来记录/对象属性来记录原来的高度
+    if ((oHeight - nHeight) > (oHeight / 3)) {
+      // 当虚拟键盘弹起时 导航栏隐藏
+      thiz.setState({
+        hidden: true
+      })
+    } else {
+      // 恢复导航栏的显示
+      thiz.setState({
+        hidden: false
+      })
+    }
+  }
+  /**
    * 当type为‘navBar’时，并且屏幕可用尺寸变化超过1/3时 则隐藏底部的导航栏
    * 1- 针对底部navBar会被虚拟键盘顶起来
+   * 2- 对于绑定的事件 - 在componentWillUnmount钩子中需要解绑
    */
   init () {
     const thiz = this
@@ -57,28 +75,19 @@ class NavBar extends React.Component {
       let ua = navigator.userAgent.toLowerCase()
       // 以防万一 这里需要判断是否在移动端 在移动端才绑定这个属性
       if (/mobile|android|iphone|phone|ipad|/i.test(ua)) {
-        // 记录原来的尺寸
-        // let oHeight = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-        window.onresize = function () {
-          const nHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-          // 当屏幕的尺寸（宽/高）变化超过1/3 -- 现在只用高 -- 这里的比较还是需要采用其他值/或者公共变量来记录/对象属性来记录原来的高度
-          if ((oHeight - nHeight) > (oHeight / 3)) {
-            // 当虚拟键盘弹起时 导航栏隐藏
-            thiz.setState({
-              hidden: true
-            })
-          } else {
-            // 恢复导航栏的显示
-            thiz.setState({
-              hidden: false
-            })
-          }
-        }
+        /** 最初的屏幕可用尺寸 -- 测试 */
+        const oHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+        // 对于在React中的事件增加我们不能用addEventListener而应该用最初的那些事件绑定方式进行绑定
+        window.onresize = () => this.hideNavBar(thiz, oHeight)
       }
     }
   }
   componentDidMount () {
     this.init()
+  }
+  componentWillUnmount () {
+    // 解绑呼出虚拟键盘后的事件 -- 因为移动端大都是高阶浏览器 这里就不用兼容ie567的写法了 -- 貌似没有执行啊
+    window.removeEventListener('onresize', this.hideNavBar)
   }
   render () {
     return (
--- src/js/components/tabBar/TabBar.jsx
+++ src/js/components/tabBar/TabBar.jsx
@@ -5,10 +5,15 @@
  * }
  * 3- 尽量减少配置 提供成熟的默认配置方案提供点击亮起的样式
  * 4- 设置默认的Nav --
- * 5- 实现页面下滑 -- tabBar向下移动一定的距离 向上
- * -- 这里我们需要在组件内监听onScroll事件 + 并且要配合一定的过渡动画 --- css3中的transition
+ * 5- 实现页面上滑 -- TabBar隐藏 下滑 则 下拉出来
+ * -- 这里我们需要在组件内监听onScroll事件 + 并且要配合一定的过渡动画 --- css3中的transition -- 这里用简单粗暴的display:none视觉体验肯定不好
  * -- 前提是type=tabBar
  * -- 越来越多的趋势指向：NavBar和TabBar分开：嗯 组件划分的粒度很重要 分吧
+ * -- 这个后期根据需要 可以一个开关量来管理
+ * 1- 实现上划隐藏/下滑显示 -- 绑定onscroll事件 -- 这个onscroll需要绑定在内容部分啊喂--看来只能全局监听手势操作了吗
+ * 2- eg:window.onscroll或document.onscroll是在浏览器滚动条滚动时触发的
+ * 3- 对于绑定的事件 - 在componentWillUnmount钩子中需要解绑 -- 特别是绑定到全局对象上的事件
+ * -- 暂时等手势事件封装完成吧
  */
 import React from 'react'
 import PropTypes from 'prop-types'
